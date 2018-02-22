754fc9e29c0475bb78d38ed33de6c97d1bcf575b
Grace Smith <gracefusako@gmail.com>
2018-02-22 16:30:21 -0700

    initial commit

---
+++ aboutme.js
@@ -0,0 +1,6 @@
+var paragraph = document.getElementById("paragraph")
+var start = document.getElementById("start")
+function changeText() {
+    console.log("clicked")
+    paragraph.innerHTML = "Great Job!"
+}
